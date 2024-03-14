dc98987cb8e4cca86c1a286edcfd280d24780f78
Soldier_RMB <soldierrmb0510@outlook.com>
2024-03-14 17:24:13 +0800

    update models

--- src/main/java/com/soldiersoft/traveler/mapper/StreetMapper.java
+++ src/main/java/com/soldiersoft/traveler/mapper/StreetMapper.java
@@ -4,11 +4,11 @@ import com.github.yulichang.base.MPJBaseMapper;
 import com.soldiersoft.traveler.entity.Street;
 
 /**
- * @author Soldier_RMB
- * @description 针对表【t_streets(乡级表)】的数据库操作Mapper
- * @createDate 2024-03-01 17:03:19
- * @Entity com.soldiersoft.traveler.entity.Street
- */
+* @author Soldier_RMB
+* @description 针对表【t_streets(乡级表)】的数据库操作Mapper
+* @createDate 2024-03-14 16:52:55
+* @Entity com.soldiersoft.traveler.entity.Street
+*/
 public interface StreetMapper extends MPJBaseMapper<Street> {
 
 }
