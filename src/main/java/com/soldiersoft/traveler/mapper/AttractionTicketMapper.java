421e00fdf626a705b45594a7220f101f93452426
Soldier_RMB <soldierrmb0510@outlook.com>
2024-04-22 21:16:45 +0800

    Remove deleteAttractionTicketsByAttractionId

--- src/main/java/com/soldiersoft/traveler/mapper/AttractionTicketMapper.java
+++ src/main/java/com/soldiersoft/traveler/mapper/AttractionTicketMapper.java
@@ -4,11 +4,11 @@ import com.github.yulichang.base.MPJBaseMapper;
 import com.soldiersoft.traveler.entity.AttractionTicket;
 
 /**
-* @author Soldier_RMB
-* @description 针对表【t_attraction_ticket(景点门票表)】的数据库操作Mapper
-* @createDate 2024-04-01 21:06:21
-* @Entity com.soldiersoft.traveler.entity.AttractionTicket
-*/
+ * @author Soldier_RMB
+ * @description 针对表【t_attraction_ticket(景点门票表)】的数据库操作Mapper
+ * @createDate 2024-04-01 21:06:21
+ * @Entity com.soldiersoft.traveler.entity.AttractionTicket
+ */
 public interface AttractionTicketMapper extends MPJBaseMapper<AttractionTicket> {
 
 }
--- src/main/java/com/soldiersoft/traveler/service/AttractionTicketService.java
+++ src/main/java/com/soldiersoft/traveler/service/AttractionTicketService.java
@@ -7,13 +7,12 @@ import com.soldiersoft.traveler.model.dto.AttractionTicketDTO;
 import java.util.List;
 
 /**
-* @author Soldier_RMB
-* @description 针对表【t_attraction_ticket(景点门票表)】的数据库操作Service
-* @createDate 2024-04-01 21:06:21
-*/
+ * @author Soldier_RMB
+ * @description 针对表【t_attraction_ticket(景点门票表)】的数据库操作Service
+ * @createDate 2024-04-01 21:06:21
+ */
 public interface AttractionTicketService extends IService<AttractionTicket> {
 
     List<AttractionTicketDTO> getAttractionTicketsByAttractionId(Long attractionId);
 
-    int deleteAttractionTicketsByAttractionId(Long attractionId);
 }
