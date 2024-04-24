372f07dd86d78bd2570d75780eb934c253e96694
Soldier_RMB <soldierrmb0510@outlook.com>
2024-04-24 15:51:36 +0800

    Jackson serialize long to string

---
+++ src/main/java/com/soldiersoft/traveler/config/JacksonConfig.java
@@ -0,0 +1,14 @@
+package com.soldiersoft.traveler.config;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return builder -> builder.serializerByType(Long.class, new ToStringSerializer());
+    }
+}
