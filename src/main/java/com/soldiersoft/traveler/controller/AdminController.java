1311f0263b55b7d5469c5e1aece3575c08f18c01
Soldier_RMB <soldierrmb0510@outlook.com>
2024-05-03 20:58:44 +0800

    Add completeDeletePost, getComments, completeDeleteComment, publishAnnouncement, updateAnnouncement, completeDeleteAnnouncement

--- src/main/java/com/soldiersoft/traveler/controller/AdminController.java
+++ src/main/java/com/soldiersoft/traveler/controller/AdminController.java
@@ -1,14 +1,13 @@
 package com.soldiersoft.traveler.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.soldiersoft.traveler.model.dto.CommentDTO;
 import com.soldiersoft.traveler.model.dto.OrderDTO;
 import com.soldiersoft.traveler.model.dto.UserRoleDTO;
+import com.soldiersoft.traveler.model.vo.AnnouncementVO;
 import com.soldiersoft.traveler.model.vo.ResultVO;
 import com.soldiersoft.traveler.model.vo.UserAttractionVO;
-import com.soldiersoft.traveler.service.AttractionService;
-import com.soldiersoft.traveler.service.OrderService;
-import com.soldiersoft.traveler.service.UserAttractionService;
-import com.soldiersoft.traveler.service.UserRoleService;
+import com.soldiersoft.traveler.service.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +24,19 @@ public class AdminController {
     private final UserAttractionService userAttractionService;
     private final OrderService orderService;
     private final UserRoleService userRoleService;
+    private final PostService postService;
+    private final CommentService commentService;
+    private final AnnouncementService announcementService;
 
     @Autowired
-    public AdminController(AttractionService attractionService, UserAttractionService userAttractionService, OrderService orderService, UserRoleService userRoleService) {
+    public AdminController(AttractionService attractionService, UserAttractionService userAttractionService, OrderService orderService, UserRoleService userRoleService, PostService postService, CommentService commentService, AnnouncementService announcementService) {
         this.attractionService = attractionService;
         this.userAttractionService = userAttractionService;
         this.orderService = orderService;
         this.userRoleService = userRoleService;
+        this.postService = postService;
+        this.commentService = commentService;
+        this.announcementService = announcementService;
     }
 
     @Operation(description = "批量审核景点")
@@ -68,5 +73,41 @@ public class AdminController {
     @GetMapping("/getUserRoles")
     public ResultVO<Page<UserRoleDTO>> getUserRoles(@RequestParam Long current, @RequestParam Long size) {
         return ResultVO.ok(userRoleService.getUserRoles(current, size));
+    }
+
+    @Operation(description = "彻底删除动态信息")
+    @DeleteMapping("/completeDeletePost")
+    public ResultVO<String> completeDeletePost(@RequestParam Long postId) {
+        return ResultVO.ok(postService.completeDeletePost(postId));
+    }
+
+    @Operation(description = "获取所有评论信息")
+    @GetMapping("/getComments")
+    public ResultVO<Page<CommentDTO>> getComments(@RequestParam Long current, @RequestParam Long size) {
+        return ResultVO.ok(commentService.getComments(current, size));
+    }
+
+    @Operation(description = "彻底删除评论信息")
+    @DeleteMapping("/completeDeleteComment")
+    public ResultVO<String> completeDeleteComment(@RequestParam Long commentId) {
+        return ResultVO.ok(commentService.completeDeleteComment(commentId));
+    }
+
+    @Operation(description = "发布系统公告信息")
+    @PostMapping("/publishAnnouncement")
+    public ResultVO<String> publishAnnouncement(@RequestBody AnnouncementVO announcementVO) {
+        return ResultVO.ok(announcementService.publishAnnouncement(announcementVO));
+    }
+
+    @Operation(description = "更新系统公告信息")
+    @PutMapping("/updateAnnouncement")
+    public ResultVO<String> updateAnnouncement(@RequestBody AnnouncementVO announcementVO) {
+        return ResultVO.ok(announcementService.updateAnnouncement(announcementVO));
+    }
+
+    @Operation(description = "彻底删除系统公告信息")
+    @DeleteMapping("/completeDeleteAnnouncement")
+    public ResultVO<String> completeDeleteAnnouncement(@RequestParam Long announcementId) {
+        return ResultVO.ok(announcementService.completeDeleteAnnouncement(announcementId));
     }
 }
